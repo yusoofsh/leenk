7c90f6059af039eb3d5d27136fc29b5fe74e3721
Yusoof Moh <18055365+yusoofsh@users.noreply.github.com>
2023-12-12 07:46:26 +0700

    chore: remove Tailwind typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
-import typography from "@tailwindcss/typography";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -11,5 +10,5 @@ export default {
       },
     },
   },
-  plugins: [typography()],
+  plugins: [],
 } satisfies Config;
