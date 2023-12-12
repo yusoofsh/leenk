b0fb6741048ef0e7ba41e775d866c38b02851f14
Yusoof Moh <18055365+yusoofsh@users.noreply.github.com>
2023-12-12 08:40:08 +0700

    feat: update Astro configuration and package dependencies
    Update the Astro configuration to set the output as "server" and use the
    Cloudflare adapter. Additionally, the "@astrojs/cloudflare" package is
    added to the dependencies. These changes introduce support for
    server-side rendering and integration with Cloudflare, improving the
    performance and scalability of the application.

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,10 @@
 import { type AstroUserConfig as Config } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default {
+  output: "server",
+  adapter: cloudflare(),
   integrations: [tailwind()],
 } satisfies Config;
