56500c9da05b5f8b2531df17096f23bcbb3df6b5
Yusoof Moh <me@yusoofsh.id>
2024-01-17 23:22:21 +0700

    feat: unique texts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,10 @@
+import typography from "@tailwindcss/typography";
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  plugins: [],
+  plugins: [typography],
   theme: {
     extend: {
       fontFamily: {
